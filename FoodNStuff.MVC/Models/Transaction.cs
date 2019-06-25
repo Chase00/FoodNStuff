8127e3a776bdc2928eee2970cf2c4f2399c6deaf
Chase Kuhn <chasekuhnmc@gmail.com>
2019-06-25 11:18:46 -0400

    Added classes

---
+++ FoodNStuff.MVC/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FoodNStuff.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+
+        [ForeignKey("Customer")] // Calling the name
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        [ForeignKey(nameof(Product))] // Get the name from the property
+        public int ProductID { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
